609e8675b82e505d8c997ec46d53f6143638dd3a
Apple Juice <mallari23adrian@gmail.com>
2022-09-30 17:45:13 +0800

    Initial commit with password encryption

---
+++ app.js
@@ -0,0 +1,114 @@
+require('dotenv').config(); //just require it, must always be on top
+const express = require("express");
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption'); //for the ability to encrypt plain text 
+
+const app = express();
+
+console.log(process.env.API_KEY);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.get("/", function(req, res){
+    res.render("home");
+}); 
+
+app.route("/login")
+.get(function(req, res){
+    res.render("login");
+})
+.post(function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if (err) {
+            console.log(err);
+        } else {
+            if(foundUser) {
+                if(foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+});
+
+app.route("/register")
+.get(function(req, res){
+    res.render("register");
+})
+.post(function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.");
+});
+
+
+
+// For registering new user
+
+// app.post("resgisterRoute", function(req, res){
+//     const newUser = new User({
+//         email: req.body.username,
+//         password: req.body.password
+//     });
+
+//     newUser.save(function(err){
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.render("secrets");
+//         }
+//     });
+// });
+
+// For authenticating existing user
+
+// app.post("route", function(req, res){
+//     const username = req.body.username;
+//     const password = req.body.password;
+
+//     User.findOne({email: username}, function(err, foundUser){
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             if(foundUser) {
+//                 if(foundUser.password === password) {
+//                     res.render("encryptedRoute");
+//                 }
+//             }
+//         }
+//     })
+// });
