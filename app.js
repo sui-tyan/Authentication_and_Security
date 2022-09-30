44982fee823ce56d91dd9ef946b0f7f2c81997da
Apple Juice <mallari23adrian@gmail.com>
2022-09-30 19:18:36 +0800

    Added md5 hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption'); //for the ability to encrypt plain text 
+const md5 = require('md5');
 
 const app = express();
 
@@ -20,7 +20,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,7 +35,7 @@ app.route("/login")
 })
 .post(function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if (err) {
@@ -58,7 +57,7 @@ app.route("/register")
 .post(function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
