36ede9baa1dd84d6c81d9d9baf9cb60f674b39fd
Apple Juice <mallari23adrian@gmail.com>
2022-09-30 23:46:16 +0800

    Code refactoring (login still have bugs)

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
@@ -14,12 +15,23 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+    secret: "Our little secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 
 const User = new mongoose.model("User", userSchema);
@@ -35,24 +47,7 @@ app.route("/login")
     res.render("login");
 })
 .post(function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, function(err, foundUser){
-        if (err) {
-            console.log(err);
-        } else {
-            if(foundUser) {
-                bcrypt.compare(password, foundUser.password, function(err, result){
-                    if(!err) {
-                        if(result === true) {
-                            res.render("secrets");
-                        }
-                    }
-                });
-            }
-        }
-    });
+    
 });
 
 app.route("/register")
@@ -61,19 +56,7 @@ app.route("/register")
 })
 .post(function(req, res){
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save(function(err){
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        });
-    });
+    
 });
 
 
